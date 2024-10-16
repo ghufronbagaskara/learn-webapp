e84ad083e97f1839f2dfbf800bd4de20b0ef5e30
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2024-10-17 01:33:14 +0700

    lesgo js for the first time

---
+++ javascript/module-2-var/index.js
@@ -0,0 +1,3 @@
+var umur = 20 // variabel yang dapat diubah dan dapat diakses diluar scopenya
+let nama = "Ghufron Bagaskara" // variabel dapat diubah, hanya diakses dalam scope nya 
+const pi = 3.14  // tidak bisa diubah, seperti layaknya konstanta
---
+++ javascript/module-3-datatype/index.js
@@ -0,0 +1,12 @@
+const nama = "Ghufron Bagaskara";
+console.log(nama, typeof nama);
+
+const negara = "Indonesia";
+const domisili = `From ${negara} an country`;
+console.log(domisili);
+
+const umur = 20
+
+const menikah = false
+
+let x;  //undefined
