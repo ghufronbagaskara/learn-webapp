82fdd7d453852d8446b24953d5d6f936b796b8df
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2026-02-27 04:29:48 +0700

    feat: implement models for rapid app & master data project

---
+++ laravel/rapid-app-dev-master-data/app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'sku', 'price', 'stock'];
+
+    public function purchaseItems(): HasMany
+    {
+        return $this->hasMany(PurchaseItem::class);
+    }
+
+    public function saleItems(): HasMany
+    {
+        return $this->hasMany(SaleItem::class);
+    }
+}
