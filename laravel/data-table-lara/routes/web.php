17b64c44c33a3f16110c622a960f1d36682be182
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-03-10 19:44:49 +0700

    feat: add products route to web php

--- laravel/data-table-lara/routes/web.php
+++ laravel/data-table-lara/routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
 Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
+  'canRegister' => Features::enabled(Features::registration()),
 ])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+  Route::inertia('dashboard', 'dashboard')->name('dashboard');
+
+  Route::get('products/datatable', [ProductController::class, 'datatable'])->name('products.datatable');
+  Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
+  Route::resource('products', ProductController::class)->except('show', 'create', 'edit');
 });
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
