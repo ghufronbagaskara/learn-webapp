000f6b18d9ecae9ab2bf60118345f6536ff0802d
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-03-12 00:33:23 +0700

    chore: update settings routes

--- laravel/data-table-lara/routes/settings.php
+++ laravel/data-table-lara/routes/settings.php
@@ -6,23 +6,23 @@ use App\Http\Controllers\Settings\TwoFactorAuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', '/settings/profile');
+  Route::redirect('settings', '/settings/profile');
 
-    Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('settings/password', [PasswordController::class, 'edit'])->name('user-password.edit');
+  Route::get('settings/password', [PasswordController::class, 'edit'])->name('user-password.edit');
 
-    Route::put('settings/password', [PasswordController::class, 'update'])
-        ->middleware('throttle:6,1')
-        ->name('user-password.update');
+  Route::put('settings/password', [PasswordController::class, 'update'])
+    ->middleware('throttle:6,1')
+    ->name('user-password.update');
 
-    Route::inertia('settings/appearance', 'settings/appearance')->name('appearance.edit');
+  Route::inertia('settings/appearance', 'settings/appearance')->name('appearance.edit');
 
-    Route::get('settings/two-factor', [TwoFactorAuthenticationController::class, 'show'])
-        ->name('two-factor.show');
+  Route::get('settings/two-factor', [TwoFactorAuthenticationController::class, 'show'])
+    ->name('two-factor.show');
 });
