d153b586ec423f9c6722751a91a727b12aceda31
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-03-21 23:28:42 +0700

    feat(auth-middleware): validate bearer jwt tokens

--- laravel/lara-lumen/app/Http/Middleware/Authenticate.php
+++ laravel/lara-lumen/app/Http/Middleware/Authenticate.php
@@ -1,42 +1,34 @@
-<?php
+<?php // app/Http/Middleware/Authenticate.php
 
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Contracts\Auth\Factory as Auth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class Authenticate
 {
-    /**
-     * The authentication guard factory instance.
-     *
-     * @var \Illuminate\Contracts\Auth\Factory
-     */
-    protected $auth;
-
-    /**
-     * Create a new middleware instance.
-     *
-     * @param  \Illuminate\Contracts\Auth\Factory  $auth
-     * @return void
-     */
-    public function __construct(Auth $auth)
+  public function handle($request, Closure $next)
     {
-        $this->auth = $auth;
-    }
+    try {
+      $user = JWTAuth::parseToken()->authenticate();
 
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
-     * @return mixed
-     */
-    public function handle($request, Closure $next, $guard = null)
-    {
-        if ($this->auth->guard($guard)->guest()) {
-            return response('Unauthorized.', 401);
+      if (!$user) {
+        return response()->json([
+          'success' => false,
+          'message' => 'Token tidak valid',
+          'errors' => null,
+        ], 401);
+      }
+
+      auth()->setUser($user);
+      $request->setUserResolver(static fn() => $user);
+    } catch (JWTException $e) {
+      return response()->json([
+        'success' => false,
+        'message' => 'Token tidak ada, invalid, atau expired',
+        'errors' => $e->getMessage(),
+      ], 401);
         }
 
         return $next($request);
