e05d6263ef9c30642438219359d50cddcd82aa54
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-04-06 06:29:23 +0700

    feat(routes): register penduduk CRUD and export routes

--- laravel/lara-php-spreadsheet/routes/web.php
+++ laravel/lara-php-spreadsheet/routes/web.php
@@ -1,5 +1,17 @@
 <?php
 
+use App\Http\Controllers\PendudukController;
+use App\Livewire\Penduduk\Create;
+use App\Livewire\Penduduk\Edit;
+use App\Livewire\Penduduk\Index;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
+Route::get('/', fn() => redirect()->route('penduduk.index'));
+
+Route::get('/penduduk', Index::class)->name('penduduk.index');
+Route::get('/penduduk/create', Create::class)->name('penduduk.create');
+Route::get('/penduduk/{penduduk}/edit', Edit::class)->name('penduduk.edit');
+
+Route::get('/penduduk/export/{format}', [PendudukController::class, 'export'])
+  ->name('penduduk.export')
+  ->where('format', 'csv|xls|xlsx');
