db89711647e00d67623195814a515a331342e8b1
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-04-06 06:12:48 +0700

    feat(lara-oauth-twofactor-seo): add edit.blade.php

---
+++ laravel/lara-oauth-twofactor-seo/resources/views/blog/comments/edit.blade.php
@@ -0,0 +1,6 @@
+<x-layouts::app :title="__('Edit Komentar')">
+    <div class="mx-auto max-w-3xl rounded-xl border border-zinc-200 bg-white p-6 dark:bg-zinc-900">
+        <h1 class="text-xl font-bold">Edit Komentar</h1>
+        <p class="mt-3 text-sm text-zinc-600">Moderasi komentar dilakukan dari halaman daftar komentar.</p>
+    </div>
+</x-layouts::app>
