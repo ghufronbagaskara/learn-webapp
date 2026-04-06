fb241cbb234102c8783216098ada7fd50ff09e17
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-04-07 00:52:36 +0700

    feat(report-data-lara): update resources/js/app.js

---
+++ laravel/report-data-lara/resources/js/app.js
@@ -0,0 +1,12 @@
+import $ from "jquery";
+import flatpickr from "flatpickr";
+import "select2";
+import { Chart } from "chart.js/auto";
+import "pivottable/dist/pivot";
+import "./reports/sales-report";
+import "./reports/pivot-report";
+
+window.$ = $;
+window.jQuery = $;
+window.flatpickr = flatpickr;
+window.Chart = Chart;
