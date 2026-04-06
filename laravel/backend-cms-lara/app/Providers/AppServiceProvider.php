5f32ac5b914c75548c782558176a9dbd3514cc2d
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-04-07 00:52:26 +0700

    chore(backend-cms-lara): update app/Providers/AppServiceProvider.php

--- laravel/backend-cms-lara/app/Providers/AppServiceProvider.php
+++ laravel/backend-cms-lara/app/Providers/AppServiceProvider.php
@@ -2,9 +2,18 @@
 
 namespace App\Providers;
 
+use App\Models\Page;
+use App\Models\Post;
+use App\Models\Survey;
+use App\Models\User;
+use App\Policies\PagePolicy;
+use App\Policies\PostPolicy;
+use App\Policies\SurveyPolicy;
+use App\Policies\UserPolicy;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -31,7 +40,12 @@ class AppServiceProvider extends ServiceProvider
      */
     protected function configureDefaults(): void
     {
-        Date::use(CarbonImmutable::class);
+    Gate::policy(Page::class, PagePolicy::class);
+    Gate::policy(Post::class, PostPolicy::class);
+    Gate::policy(Survey::class, SurveyPolicy::class);
+    Gate::policy(User::class, UserPolicy::class);
+
+    Date::use(CarbonImmutable::class);
 
         DB::prohibitDestructiveCommands(
             app()->isProduction(),
