d48e7ae4afa38b25a3bb3ca7f68978bddf3cd3db
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-04-07 00:52:35 +0700

    feat(report-data-lara): update database/seeders/DatabaseSeeder.php

--- laravel/report-data-lara/database/seeders/DatabaseSeeder.php
+++ laravel/report-data-lara/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+    $users = User::factory(25)->create();
 
-        User::factory()->create([
+    $testUser = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+    Order::factory(320)
+      ->recycle($users->push($testUser))
+      ->create();
     }
 }
